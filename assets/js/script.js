590d120cb8bd03073fa7b3d4756dae0314015fe2
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-10-18 12:22:33 -0400

    Added basic variables to Javascript and set up event listener.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 ticketmasterApi = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=FwyMEHGWc3ybkab0m3FG8jMPqqlKi5QP"
+
+// declaring variables
+searchInputEl = document.getElementById("search-input");
+searchButtonEl = document.getElementById("search-button");
+
+// event listener for when search button is pressed
+searchButtonEl.addEventListener("click", function() {
+    console.log("button was pressed");
+});
