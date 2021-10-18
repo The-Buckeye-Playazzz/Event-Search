baa21884a296b788fb4c1ea2b85747018da7a081
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-10-18 12:53:54 -0400

    Added basic function to input search term into Ticketmaster API url.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-ticketmasterApi = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=FwyMEHGWc3ybkab0m3FG8jMPqqlKi5QP"
 
 // declaring variables
 searchInputEl = document.getElementById("search-input");
 searchButtonEl = document.getElementById("search-button");
 
+// defining primary function
+function searchNearbyEvents() {
+    console.log(searchInputEl.value);
+    ticketmasterUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + searchInputEl.value + "&apikey=FwyMEHGWc3ybkab0m3FG8jMPqqlKi5QP";
+
+    console.log(ticketmasterUrl)
+}
+
 // event listener for when search button is pressed
-searchButtonEl.addEventListener("click", function() {
-    console.log("button was pressed");
-});
+searchButtonEl.addEventListener("click", searchNearbyEvents);
