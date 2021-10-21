a805360dd662632e2cc11c3b0b7f208d720ea0ae
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-10-21 11:37:15 -0400

    Formatted time of each event if one is given.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,21 @@
 searchInputEl = document.getElementById("search-input");
 searchButtonEl = document.getElementById("search-button");
 resultsContainerEl = document.querySelector(".results-container");
-weatherContainer = document.querySelector(".weather-container");             
-var far;
-var dscptn;
+weatherContainerEl = document.querySelector(".weather-container");             
 
 
 // function to display nearby events
 function searchNearbyEvents() {
     var currentDate = moment().format("YYYY-MM-DD");
-    ticketmasterUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + searchInputEl.value + "&startDateTime=" + currentDate + "T00:00:00Z&sort=date,asc&apikey=FwyMEHGWc3ybkab0m3FG8jMPqqlKi5QP";
+    var ticketmasterUrl = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + searchInputEl.value + "&startDateTime=" + currentDate + "T00:00:00Z&sort=date,asc&apikey=FwyMEHGWc3ybkab0m3FG8jMPqqlKi5QP";
 
     fetch(ticketmasterUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+
+            // create and display current events
             for (var i = 0; i < data._embedded.events.length; i++) {
                 var createName = document.createElement("h2");
                 var createDate = document.createElement("p");
@@ -25,9 +25,13 @@ function searchNearbyEvents() {
 
                 createName.textContent = data._embedded.events[i].name;
                 createDate.textContent = data._embedded.events[i].dates.start.localDate;
-                createTime.textContent = data._embedded.events[i].dates.start.localTime;
                 createLink.textContent = "Purchase tickets";
                 createLink.href = data._embedded.events[i].url;
+
+                // formats time of event if there is one
+                if (data._embedded.events[i].dates.start.localTime) {
+                    createTime.textContent = moment(data._embedded.events[i].dates.start.localTime, "HH:mm:ss").format("hh:mm A"); 
+                }
 
                 createName.appendChild(createDate);
                 createName.appendChild(createTime);
@@ -47,9 +51,10 @@ function displayWeather() {
     })
     .then(function (data) {
 
+        // create and display current weather information
         var far = data.main.temp;
         var temperature = document.createElement("p");
-        temperature.textContent = "The Temperature Today is " + far + " deg's Farenheit";
+        temperature.textContent = "The temperature today is " + far + " °F";
 
         var dscptn = data.weather[0].description;
         var desc = document.createElement("p");
@@ -57,18 +62,18 @@ function displayWeather() {
 
         var feelsLike = data.main.feels_like;
         var flsLike = document.createElement("p");
-        flsLike.textContent = feelsLike + " deg's Farenheit Outside";
+        flsLike.textContent = feelsLike + " °F outside.";
 
-        weatherContainer.appendChild(temperature);
-        weatherContainer.appendChild(desc);
-        weatherContainer.appendChild(flsLike);
+        weatherContainerEl.appendChild(temperature);
+        weatherContainerEl.appendChild(desc);
+        weatherContainerEl.appendChild(flsLike);
     })
 }
 
 // function to clear previous search upon a new search
 function clearContent () {
     resultsContainerEl.innerHTML = "";
-    weatherContainer.innerHTML = "";
+    weatherContainerEl.innerHTML = "";
 }
 
 // event listener for when search button is pressed
